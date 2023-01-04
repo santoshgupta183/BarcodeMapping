9b5e4fdd72ff69a3fbb11ba8a873d3d2d497a37d
Santosh <santosh.gupta183@gmail.com>
2023-01-04 22:39:18 +0530

    removed unused code & minor UI changes

---
+++ app/src/main/java/com/qikkle/barcodemapping/utils/APIConstants.kt
@@ -0,0 +1,12 @@
+package com.qikkle.barcodemapping.utils
+
+class APIConstants {
+    companion object {
+        const val LOGIN = "Identity/User/ValidateLoginUser"
+        const val BARCODE_DETAILS = "Essential/Asset/GetDetailbySerialNo"
+        const val CREATE_ASSET = "Essential/Asset/CreateAsset"
+        const val GET_CATEGORY = "Essential/Category/GetAllCategorys"
+        const val GET_PRODUCT = "Essential/Asset/GetAllProductList"
+        const val GET_RECEIVERS = "Identity/User/GetAllUsers"
+    }
+}
